91deb4b91071d7705ef1478ea29df80623bed56c
Mileshosky <mileshosky+britt@gmail.com>
2014-04-14 23:07:31 -0500

    Resque.redis hasn't been initialized yet.

--- lib/resque-delayed/tasks.rb
+++ lib/resque-delayed/tasks.rb
@@ -5,16 +5,6 @@ namespace :resque_delayed do
 
   desc "Start a Resque::Delayed worker"
   task :work do
-    unless Resque.redis.instance_variable_get(:@redis).zcard("").zero?
-      STDERR.puts %Q{
-WARNING: you have a sorted set stored at the empty string key in your redis instance
-if you've just upgraded from Resque::Delayed 1.0.0 you probably want to run
-
- $ bundle exec rake resque_delayed:migrate_queue key
-
-see resque-delayed/CHANGELOG.md for details}
-    end
-
     begin
       worker = Resque::Delayed::Worker.new
       worker.verbose = ENV['LOGGING'] || ENV['VERBOSE']
